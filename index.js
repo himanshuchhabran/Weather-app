607cb2073dae1a900c1897af74dad5931d7187b8
himanshuchhabran <himanshuchhabran@gmail.com>
2024-01-03 10:23:21 +0530

    project started

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 async function showWeather(){
-    const API_key = "f8fd70913c79183721e09e44fdb376fa";
+    const API_key = "#";
     let latitude = 28.4267;
     let longitude = 75.8040;
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=28.4267&lon=75.8040&appid=f8fd70913c79183721e09e44fdb376fa`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=28.4267&lon=75.8040&appid=#`);
 
     const data = await response.json();
     console.log("Weather data:->" ,data); 
