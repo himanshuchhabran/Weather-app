48f571278f647e4ae7212a713f77573277f61e69
himanshuchhabran <himanshuchhabran@gmail.com>
2024-01-04 09:32:28 +0530

    html structure done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-async function showWeather(){
-    const API_key = "#";
-    let latitude = 28.4267;
-    let longitude = 75.8040;
+// async function showWeather(){
+//     const API_key = "#";
+//     let latitude = 28.4267;
+//     let longitude = 75.8040;
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=28.4267&lon=75.8040&appid=#`);
+//     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=28.4267&lon=75.8040&appid=#`);
 
-    const data = await response.json();
-    console.log("Weather data:->" ,data); 
+//     const data = await response.json();
+//     console.log("Weather data:->" ,data); 
 
-    let newPara = document.createElement('p');
-    newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
-    document.body.appendChild(newPara);
-}
+//     let newPara = document.createElement('p');
+//     newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
+//     document.body.appendChild(newPara);
+// }
 
-showWeather();
+// showWeather();
